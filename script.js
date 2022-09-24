730f9216d6516415105cc652cc93cbdf66296ad3
KennySabee <kemerizalde.dwfsc10@ucamp.utel.edu.mx>
2022-09-24 09:13:51 -0500

    empezando js

---
+++ script.js
@@ -0,0 +1,24 @@
+let listaEmpleados= []
+const objEmpleado = {
+    id: '',
+    nombre: '',
+    puesto: '',
+
+}
+let editando = false;
+
+const formulario = document.querySelector('#formulario')
+const nombreInput = document.querySelector('#nombre')
+const puestoInput = document.querySelector('#puesto')
+const btnAgregar = document.querySelector('#btnAgregar')
+
+formulario.addEventListener('submit', validarFormulario);
+
+function validarFormulario(e) {
+    e.preventDefault();
+
+    if(nombreInput.value === '' || puestoInput.value === '') {
+        alert('Todos los campos son obligatorios')
+        return;
+    }
+}
